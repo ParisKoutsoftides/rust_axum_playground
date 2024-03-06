ed6aff579dee9d78c12a4608d6144361ca7e5587
Paris <pariskoutso@hotmail.co.uk>
2024-03-06 07:30:37 +0200

    add json route

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
 use axum::{
     routing::get,
     Router,
+    response::Json,
+};
+
+use serde_json::{
+    Value,
+    json,
 };
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
         .route("/", get(hello_world))
-        .route("/testerino", get(testerino));
+        .route("/testerino", get(testerino))
+        .route("/json", get(json));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
@@ -20,4 +27,8 @@ async fn hello_world() -> &'static str {
 
 async fn testerino() -> &'static str {
     "Testerino!"
+}
+
+async fn json() -> Json<Value> {
+    Json(json!({ "data": 42 }))
 }
