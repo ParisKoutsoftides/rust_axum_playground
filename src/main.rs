849af386581300728e433a58e65ef8c91ca5b2a4
Paris <pariskoutso@hotmail.co.uk>
2024-03-06 07:16:45 +0200

    add second route

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,9 @@ use axum::{
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .route("/testerino", get(|| async {"Testerino!"}));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
