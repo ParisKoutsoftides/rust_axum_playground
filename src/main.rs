62f53863e9de6abe0275310f1b6424d54ac5af33
Paris <pariskoutso@hotmail.co.uk>
2024-03-07 07:55:04 +0200

    playing around with a basic config store

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,8 @@ async fn main() {
         .route("/", get(hello_world))
         .route("/testerino", get(testerino))
         .route("/json", get(json))
-        .route("/query", get(query));
+        .route("/query", get(query))
+        .route("/get_config", get(get_config));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
@@ -44,4 +45,21 @@ async fn json() -> Json<Value> {
 async fn query(Query(params): Query<HashMap<String, String>>) -> Json<Value> {
     let param_testerino = params.get("paramerino").cloned().unwrap_or(String::from("empty"));
     Json(json!({ "data": param_testerino }))
+}
+
+async fn get_config(Query(params): Query<HashMap<String, String>>) -> Json<Value> {
+    let mut config_map = HashMap::new();
+    config_map.insert(
+        "configKey1".to_string(),
+        "configValue1".to_string(),
+    );
+
+    config_map.insert(
+        "configKey2".to_string(),
+        "configValue2".to_string(),
+    );
+
+    let config_key = params.get("configKey").cloned().unwrap_or(String::from("empty"));
+    let config_value = config_map.get(&config_key).cloned().unwrap_or(String::from("empty"));
+    Json(json!({ config_key: config_value }))
 }
